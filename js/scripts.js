243ed4146105076c4055c8ec415b9e18b4b45df0
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-14 00:31:46 -0700

    4

--- js/scripts.js
+++ js/scripts.js
@@ -38,14 +38,6 @@ TaskList.prototype.deleteTask = function(id) {
 
 
 
-
-
-
-
-
-
-
-
 function Task(taskName, status) {
   this.taskName = taskName;
   this.status = status;
@@ -53,14 +45,16 @@ function Task(taskName, status) {
 
 // User Interface Logic ----------
 let tasklist = new TaskList();
-let task1 = new Task("finish this thing", "nonedone");
-let task2 = new Task("finish something else", "nonedone");
-console.log(task1.status);
-tasklist.addTask(task1);
-tasklist.addTask(task2);
+let task = new Task("finish this thing", "nonedone");
+//let task2 = new Task("finish something else", "nonedone");
+console.log(task.status);
+tasklist.addTask(task);
 console.log(tasklist.numberOfTasks);
-console.log(task1.id);
-console.log(task2.id);
+console.log(task.id);
+task = new Task("test2", "nonedone");
+tasklist.addTask(task);
+console.log(tasklist.tasks[2].taskName);
+
 
 
 
@@ -68,13 +62,14 @@ $(document).ready(function() {
   $("form#side").submit(function(event) {
     event.preventDefault();
     const input = $("#side1").val();
-    let task1 = new Task(input, "nonedone");
-    let numtasks = tasklist.numberOfTasks;
-    console.log(numtasks);
-    tasklist.numberOfTasks++;
-    console.log(tasklist.numberOfTasks);
-    $("#work-responses").html('<input type="checkbox" name="tasks-to-do" value="walk">' + task1.taskName + '<br>');
-
+    let task = new Task(input, "nonedone");
+    tasklist.addTask(task);
+    for (let i = 0; i<5; i++){
+      console.log(i);
+      //$('#work-responses').append(tasklist.tasks[tasklist.numberOfTasks] + "<br>");
+      $("#work-responses").html('<input type="checkbox" name="tasks-to-do" value="'+i+'">' + tasklist.tasks[tasklist.numberOfTasks].taskName + '<br>');
+      $("#work-responses").append(workTransportationMode + "<br>")
+    }
   });
 });
 
@@ -85,6 +80,7 @@ $(document).ready(function(){
     $("input:checkbox[name=tasks-to-do]:checked").each(function(){
 
       const workTransportationMode = $(this).val();
+      console.log($(this).val());
       $('#work-responses').append(workTransportationMode + "<br>");
     });
   });
@@ -123,85 +119,85 @@ $(document).ready(function(){
 
 
 
-// Business Logic for AddressBook ---------
-function AddressBook() {
-  this.contacts = {};
-  this.currentId = 0;
-}
+// // Business Logic for AddressBook ---------
+// function AddressBook() {
+//   this.contacts = {};
+//   this.currentId = 0;
+// }
 
-AddressBook.prototype.addContact = function(contact) {
-  contact.id = this.assignId();
-  this.contacts[contact.id] = contact;
-};
+// AddressBook.prototype.addContact = function(contact) {
+//   contact.id = this.assignId();
+//   this.contacts[contact.id] = contact;
+// };
 
-AddressBook.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-};
+// AddressBook.prototype.assignId = function() {
+//   this.currentId += 1;
+//   return this.currentId;
+// };
 
-AddressBook.prototype.findContact = function(id) {
-  if (this.contacts[id] != undefined) {
-    return this.contacts[id];
-  }
-  return false;
-};
+// AddressBook.prototype.findContact = function(id) {
+//   if (this.contacts[id] != undefined) {
+//     return this.contacts[id];
+//   }
+//   return false;
+// };
 
-AddressBook.prototype.deleteContact = function(id) {
-  if (this.contacts[id] === undefined) {
-    return false;
-  }
-  delete this.contacts[id];
-  return true;
-};
+// AddressBook.prototype.deleteContact = function(id) {
+//   if (this.contacts[id] === undefined) {
+//     return false;
+//   }
+//   delete this.contacts[id];
+//   return true;
+// };
 
-// Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
-}
+// // Business Logic for Contacts ---------
+// function Contact(firstName, lastName, phoneNumber) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.phoneNumber = phoneNumber;
+// }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-};
+// Contact.prototype.fullName = function() {
+//   return this.firstName + " " + this.lastName;
+// };
 
-// User Interface Logic ---------
-let addressBook = new AddressBook();
+// // User Interface Logic ---------
+// let addressBook = new AddressBook();
 
-function displayContactDetails(addressBookToDisplay) {
-  let contactsList = $("ul#contacts");
-  let htmlForContactInfo = "";
-  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
-    const contact = addressBookToDisplay.findContact(key);
-    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
-  });
-  contactsList.html(htmlForContactInfo);
-}
+// function displayContactDetails(addressBookToDisplay) {
+//   let contactsList = $("ul#contacts");
+//   let htmlForContactInfo = "";
+//   Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+//     const contact = addressBookToDisplay.findContact(key);
+//     htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+//   });
+//   contactsList.html(htmlForContactInfo);
+// }
 
-function attachContactListeners() {
-  $("ul#contacts").on("click", "li", function() {
-    showContact(this.id);
-  });
-  $("#buttons").on("click", ".deleteButton", function() {
-    addressBook.deleteContact(this.id);
-    $("#show-contact").hide();
-    displayContactDetails(addressBook);
-  });
-}
+// function attachContactListeners() {
+//   $("ul#contacts").on("click", "li", function() {
+//     showContact(this.id);
+//   });
+//   $("#buttons").on("click", ".deleteButton", function() {
+//     addressBook.deleteContact(this.id);
+//     $("#show-contact").hide();
+//     displayContactDetails(addressBook);
+//   });
+// }
 
 
-$(document).ready(function() {
-  attachContactListeners(); 
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
-    const inputtedFirstName = $("input#new-first-name").val();
-    const inputtedLastName = $("input#new-last-name").val();
-    const inputtedPhoneNumber = $("input#new-phone-number").val();
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
-    addressBook.addContact(newContact);
-    displayContactDetails(addressBook);
-  });
-});
+// $(document).ready(function() {
+//   attachContactListeners(); 
+//   $("form#new-contact").submit(function(event) {
+//     event.preventDefault();
+//     const inputtedFirstName = $("input#new-first-name").val();
+//     const inputtedLastName = $("input#new-last-name").val();
+//     const inputtedPhoneNumber = $("input#new-phone-number").val();
+//     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+//     addressBook.addContact(newContact);
+//     displayContactDetails(addressBook);
+//   });
+// });
 
 
 
